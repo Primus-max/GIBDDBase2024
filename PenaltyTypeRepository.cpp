baefb2fa9272ea92b1ebd8ca0d03cd412fb6dcae
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-05-07 21:42:38 +0300

    CRUD для PenaltyTypeRepository

--- PenaltyTypeRepository.cpp
+++ PenaltyTypeRepository.cpp
@@ -2,6 +2,8 @@
 
 using namespace System;
 using namespace System::Collections::Generic;
+using namespace System::Data::OleDb;
+using namespace System::Windows::Forms;
 
 PenaltyTypeRepository::PenaltyTypeRepository(String^ connStr)
 {
@@ -10,23 +12,123 @@ PenaltyTypeRepository::PenaltyTypeRepository(String^ connStr)
 
 void PenaltyTypeRepository::Add(PenaltyType^ penaltyType)
 {
-    // Реализация добавления
+	OleDbConnection^ connection = gcnew OleDbConnection(_connectionString);
+	String^ queryAdd = "INSERT INTO [penalty_types] (penalty_type, price) VALUES(@penalty_type, @price)";
+	OleDbCommand^ commandAdd = gcnew OleDbCommand(queryAdd, connection);
+
+	commandAdd->Parameters->AddWithValue("@penalty_type", penaltyType->penaltyType);
+	commandAdd->Parameters->AddWithValue("@price", penaltyType->price);	
+
+	try
+	{
+		connection->Open();
+
+		if (commandAdd->ExecuteNonQuery() != 1)
+			MessageBox::Show("Не удалось добавить описание штрафа в базу", "Ошибка!");
+
+	}
+	catch (OleDbException^ ex)
+	{
+		String^ errorMessage = "Ошибка при добавлении описания штрафа в базу данных: " + gcnew String(ex->Message);
+		MessageBox::Show(errorMessage);
+	}
+	finally
+	{
+		connection->Close();
+	}
 }
 
 List<PenaltyType^>^ PenaltyTypeRepository::GetAll()
 {
-    List<PenaltyType^>^ penaltyTipes = gcnew List<PenaltyType^>();
+	List<PenaltyType^>^ penaltiesTypes = gcnew List<PenaltyType^>();
 
-    return penaltyTipes;
-    // Реализация получения всех элементов
+	OleDbConnection^ connection = gcnew OleDbConnection(_connectionString);
+	String^ queryGet = "SELECT * FROM [penalty_types]";
+	OleDbCommand^ commandRead = gcnew OleDbCommand(queryGet, connection);
+
+
+	try
+	{
+		connection->Open();
+		OleDbDataReader^ reader = commandRead->ExecuteReader();
+
+		while (reader->Read())
+		{
+			PenaltyType^ penaltyType = gcnew PenaltyType();
+
+			penaltyType->id = Convert::ToInt32(reader["id"]);
+			penaltyType->penaltyType = reader["date_p"]->ToString();
+			penaltyType->price = Convert::ToDouble(reader["amount"]);
+			
+			penaltiesTypes->Add(penaltyType);
+		}
+
+		return penaltiesTypes;
+	}
+	catch (OleDbException^ ex)
+	{
+		String^ errorMessage = "Ошибка при добавлении описания штрафа в базу данных: " + gcnew String(ex->Message);
+		MessageBox::Show(errorMessage);
+	}
+	finally
+	{
+		connection->Close();
+	}
 }
 
 void PenaltyTypeRepository::Update(PenaltyType^ penaltyType)
 {
-    // Реализация обновления
+	OleDbConnection^ connection = gcnew OleDbConnection(_connectionString);
+	//@penalty_type, @price
+	String^ queryUpdate = "UPDATE [penalty_types] SET penalty_type = @penalty_type, price = @price WHERE id = @id";
+	OleDbCommand^ commandUpdate = gcnew OleDbCommand(queryUpdate, connection);
+
+	commandUpdate->Parameters->AddWithValue("@penalty_type", penaltyType->penaltyType);
+	commandUpdate->Parameters->AddWithValue("@price", penaltyType->price);
+	commandUpdate->Parameters->AddWithValue("@id", penaltyType->id);
+
+	try
+	{
+		connection->Open();
+
+		if (commandUpdate->ExecuteNonQuery() != 1)
+			MessageBox::Show("Не удалось обновить описание штрафа в базе", "Ошибка!");
+
+	}
+	catch (OleDbException^ ex)
+	{
+		String^ errorMessage = "Ошибка при добавлении описания штрафа в базу данных: " + gcnew String(ex->Message);
+		MessageBox::Show(errorMessage);
+	}
+	finally
+	{
+		connection->Close();
+	}
 }
 
 void PenaltyTypeRepository::Delete(int penaltyTypeId)
 {
-    // Реализация удаления
+	OleDbConnection^ connection = gcnew OleDbConnection(_connectionString);
+	String^ queryDelete = "DELETE [penalty] WHERE id = @id";
+	OleDbCommand^ commandUpdate = gcnew OleDbCommand(queryDelete, connection);
+
+	commandUpdate->Parameters->AddWithValue("@id", penaltyTypeId);
+
+	try
+	{
+		connection->Open();
+
+		if (commandUpdate->ExecuteNonQuery() != 1)
+			MessageBox::Show("Не удалось удалить описание штрафа в базе", "Ошибка!");
+
+	}
+	catch (OleDbException^ ex)
+	{
+		String^ errorMessage = "Ошибка при удалении описания штрафа из базы данных: " + gcnew String(ex->Message);
+		MessageBox::Show(errorMessage);
+	}
+	finally
+	{
+		connection->Close();
+	}
 }
